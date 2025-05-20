c1d2d4c84079a5f9cd80dba571f34b36cc24a638
Franklin Souza <frankrsouza@gmail.com>
2025-05-20 14:04:22 -0300

    retirando consoles

--- assets/js/alipes.js
+++ assets/js/alipes.js
@@ -376,7 +376,6 @@ $(".contact-page__form").validate({
         return res.json();
       })
       .then(response => {
-        console.log(response);
         form.reset();
         formStatus.html('<p style="background-color: #198754; color: #fff; width: 300px; padding: 10px; border-radius: 4px; margin: 0 auto;">Mensagem enviada com sucesso!</p>');
       })
